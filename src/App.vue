58c98ba080265b4cb8c6ad96848739e5695904ea
Takuma Osada <judgment9ttk069@gmail.com>
2019-02-24 14:15:17 +0900

    make sure favicon is shown

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <div>Icons made by <a href="https://www.freepik.com/">Freepik</a> from <a href="https://www.flaticon.com/">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/">CC 3.0 BY</a></div>
   </div>
 </template>
 
