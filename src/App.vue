b148b09b319d3de7e3eafe7ba528fd92d2a321c1
Takuma Osada <judgment9ttk069@gmail.com>
2019-02-24 17:16:37 +0900

    replace slide

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
   <div id="app">
-    <Slide>
-      <router-link to="/">
-        <span>Home</span>
-      </router-link>
-      <router-link to="/">
-        <span>Timeline</span>
-      </router-link>
-      <router-link to="slide">
-        <span>Slide</span>
-      </router-link>
-    </Slide>
-    <img src="./assets/image/logo.png">
     <router-view/>
-    <div class="license">Icons made by <a href="https://www.freepik.com/">Freepik</a> from <a href="https://www.flaticon.com/">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/">CC 3.0 BY</a></div>
   </div>
 </template>
 
 <script>
-import { Slide } from 'vue-burger-menu'
-
 export default {
-  name: 'App',
-  components: {
-    Slide
-  }
+  name: 'App'
 }
 </script>
 
@@ -36,11 +18,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-.license {
-  color: black;
-}
-.license a {
-  color: #2c3e50;
 }
 </style>
--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="root">
+    <Slide>
+      <router-link to="/">
+        <span>Home</span>
+      </router-link>
+      <router-link to="/">
+        <span>Timeline</span>
+      </router-link>
+      <router-link to="slide">
+        <span>Slide</span>
+      </router-link>
+    </Slide>
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -10,7 +21,7 @@
         <ul>
           <li v-for="(info, index) in timeline_data" v-bind:key="index">
             <span></span>
-            <div>{{ info.school }}</div>
+            <div>{{ info.place }}</div>
             <div>{{ info.address }}</div>
             <div>{{ info.course }}</div>
             <div class="year">
@@ -21,32 +32,38 @@
         </ul>
       </section>
     </main>
+    <div class="license">Icons made by <a href="https://www.freepik.com/">Freepik</a> from <a href="https://www.flaticon.com/">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/">CC 3.0 BY</a></div>
   </div>
 </template>
 
 <script>
+import { Slide } from 'vue-burger-menu'
+
 export default {
   name: 'Root',
+  components: {
+    Slide
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
       timeline_data: [
         {
-          'school': 'Ateneo de Davao University',
+          'place': 'Ateneo de Davao University',
           'address': 'Roxas Ave, Poblacion District, Davao City',
           'course': 'BS in Information Technology',
           'yearStart': '2009',
           'yearEnd': '2014'
         },
         {
-          'school': 'St. Marys College',
+          'place': 'St. Marys College',
           'address': 'Daang Maharlika Highway, Tagum City',
           'course': '',
           'yearStart': '2005',
           'yearEnd': '2009'
         },
         {
-          'school': 'Magugpo Pilot Imelda Elementary School',
+          'place': 'Magugpo Pilot Imelda Elementary place',
           'address': 'Sobrecary St, Tagum City',
           'course': '',
           'yearStart': '1999',
@@ -73,5 +90,11 @@ li {
 }
 a {
   color: #42b983;
+}
+.license {
+  color: black;
+}
+.license a {
+  color: #2c3e50;
 }
 </style>
