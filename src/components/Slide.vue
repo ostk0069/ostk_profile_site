2b3173026fcc2f1694f1c752bfe303231de71c6d
Takuma Osada <judgment9ttk069@gmail.com>
2019-09-30 23:25:16 +0900

    wip

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -16,19 +16,4 @@ export default {
 </script>
 
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-  color: #42b983;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
